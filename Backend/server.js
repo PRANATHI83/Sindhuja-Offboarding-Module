a01e3e5b775c14d09c297dc82d2c77ca3a25701a
PRANATHI83 <apranu8@gmail.com>
2025-06-21 12:35:10 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3011;
+const port = 3005;
 
 // Database connection configuration
 const pool = new Pool({
     user: 'postgres',
     host: 'postgres',
     database: 'offboarding_db',
-    password: 'admin1234',
+    password: 'admin234',
     port: 5432,
 });
 
@@ -208,5 +208,5 @@ app.delete('/api/offboarding', async (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running at http://3.108.193.252:${port}`);
+    console.log(`Server running at http://13.201.36.187:${port}`);
 });
